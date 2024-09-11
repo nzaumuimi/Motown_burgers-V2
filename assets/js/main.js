61a69eee495e01f32429314a6d90eb89f13392cc
nzaumuimi <nzaumuimi@gmail.com>
2024-09-11 10:27:22 -0700

    insert title and home description

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,27 @@ const navMenu = document.getElementById('nav-menu'),
 
 /* Menu show */
 if(navToggle){
-    navToggle.addEventListener('click' , () =>{
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
 /* Menu hidden */
 if(navClose){
-    navClose.addEventListener('click' , () =>{
+    navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
-/*=============== REMOVE MENU MOBILE ===============*/
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    //When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove(show-menu)
+}
+navMenu.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
 
